c72ac7e9ecb8de7771492bb8d061a11f27cb89af
AL-KL <almousakhalil@gmail.com>
2022-09-30 20:40:25 +0200

    skel return for read func

---
+++ src/SensorData.h
@@ -0,0 +1,29 @@
+struct hostData{
+    vxxREf
+    ...
+
+
+}
+struct sensorData{
+    dx 
+    dy 
+    ....
+
+
+}
+struct cameraData{
+    dx 
+    dy 
+    ....
+
+
+}
+
+AdmaInfo{
+    
+}
+struct MeasuredData{
+    hostData host;
+    sensorData sensInfo[4][15]
+    cameradata camData
+}
