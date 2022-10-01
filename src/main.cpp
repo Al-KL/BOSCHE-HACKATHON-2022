6fb3bf3548de78f2fb1ccc08b8d981238accd75e
AL-KL <almousakhalil@gmail.com>
2022-10-01 13:32:26 +0200

    removed editor config

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,18 @@
 #define left_rear_elevation 0.48
 #define right_front_elevation 0
 #define right_rear_elevation 0.48
+#define left_front_x 3.4738
+#define left_rear_x -0.7664
+#define right_front_x 3.4738
+#define right_rear_x -0.7664
+#define left_front_y 0.6286
+#define left_rear_y -0.738
+#define right_front_y -0.6286
+#define right_rear_y -0.738
+#define left_front_z 0.5156
+#define left_rear_z 0.7359
+#define right_front_z 0.5156
+#define right_rear_z -0.7359
 
 int main(int argc, char *argv[]){
     if(argc!=3){
