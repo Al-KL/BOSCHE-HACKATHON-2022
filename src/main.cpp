1aaf98cce543f3797177ab241b2681ae51641055
AL-KL <almousakhalil@gmail.com>
2022-10-01 13:21:36 +0200

    added some defs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include "Sensor.h"
 #define left_front_azmuth 42
-#define right_front_azmuth 135
+#define left_rear_azmuth 135
+#define right_front_azmuth -42
+#define right_rear_azmuth -135
+#define left_front_elevation 0
+#define left_rear_elevation 0.48
+#define right_front_elevation 0
+#define right_rear_elevation 0.48
+
 int main(int argc, char *argv[]){
     if(argc!=3){
      std::cout<<"Wrong args";   
