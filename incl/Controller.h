10836acda26f4c9d8eea06864f239c8417e6a6e8
BMugi <batbilegmunkhorgil@gmail.com>
2022-10-01 14:45:38 +0200

    Controller class

---
+++ incl/Controller.h
@@ -0,0 +1,25 @@
+#include <vector>
+namespace sensor {
+	class Radar;
+	class Camera;
+};
+
+namespace control {
+	class Controller {
+	public:
+		static controller* get() {
+			if (theController == NULL) {
+				theController = new Controller;
+			}
+			return theController;
+		}
+		void initialise(sensor::Camera* camera; sensor::Radar* radar[4];)
+
+	private:
+		static Controller* theController;//init to null
+		sensor::Camera *camera;
+		sensor::Radar *radar[4];
+	};
+	static Controller theController = NULL;
+
+}
