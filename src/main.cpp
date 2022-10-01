65ef23c687b02fd31154036b385382a51e99cf8f
AL-KL <almousakhalil@gmail.com>
2022-10-01 14:22:15 +0200

    added camera

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,9 @@
 #define left_rear_z 0.7359
 #define right_front_z 0.5156
 #define right_rear_z -0.7359
+#define camera_x 0.17826001
+#define camera_y 0
+#define camera_z 0.14951001
 int main(int argc, char *argv[]){
     if(argc!=3){
         std::cout<<"Wrong args";   
@@ -29,6 +32,7 @@ int main(int argc, char *argv[]){
     sensor::Radar radarLR(left_rear_azmuth,left_rear_elevation,left_rear_x,left_rear_y,left_rear_z);
     sensor::Radar radarRF(right_front_azmuth,right_front_elevation,right_front_x,right_front_y,right_front_z);
     sensor::Radar radatRR(right_rear_azmuth,right_rear_elevation,right_rear_x,right_rear_y,right_rear_z);
+    sensor::Camera(camera_x,camera_y,camera_z);
     while(dataReader.isValid()){
             Data::MeasuredData measurement= dataReader.readData();
     }
